435187d3b58ec8be24763dfa2f1608efe8541e19
Selina Su <phongsu20@gmail.com>
2022-12-20 11:24:58 -0800

    ready for testing

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,3 +9,14 @@ const {
     addReaction,
     deleteReaction,
 } = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+// /api/thoughts/:id
+router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought);
+// /api/thoughts/:id/reactions
+router.route('/:id/reactions').post(addReaction);
+// /api/thoughts/:id/reactions/reactionId
+router.route('/:id/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
