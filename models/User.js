a3ac41e0b8e2f4aa315aa842a01aab50b204a6d2
Selina Su <phongsu20@gmail.com>
2022-12-19 17:52:26 -0800

    finish with userController

--- models/User.js
+++ models/User.js
@@ -7,6 +7,40 @@ const userSchema = new Schema(
         required: true,
         unique: true,
         trim: true,
-       } 
-    }
-)
+       },
+       email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address',],
+       },
+       thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+       ],
+       friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+       ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+    },
+);
+
+// Get friends count
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
